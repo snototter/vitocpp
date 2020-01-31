391ed06b75af350ba10ff6aef13b4ffd7c0976af
snototter <snototter@users.noreply.github.com>
2020-01-31 13:03:03 +0100

    clean up

--- examples/python3/tools/plot-image-sequence.py
+++ examples/python3/tools/plot-image-sequence.py
@@ -194,15 +194,6 @@ def gui():
     # Prepare example data
     rgb = imutils.imread(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..' , '..', 'data' , 'flamingo.jpg'))
     # 3-channel grayscale
-    rgb = imutils.cartoonify(rgb, num_pyramid_levels=4,
-        num_bilateral_filters=5,
-        diameter_pixel_neighborhood=7,
-        sigma_color=9,
-        sigma_space=7,
-        kernel_size_median=7,
-        edge_block_size=13,
-        is_rgb=True)
-    # rgb = imutils.pixelate(rgb, 10)
     gray_ = imutils.rgb2gray(rgb, is_bgr=False)
     gray = np.dstack((gray_, gray_, gray_))
     # 3 images with highlighted red, green, and blue channels
