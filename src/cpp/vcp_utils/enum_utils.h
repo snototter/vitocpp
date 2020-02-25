ff4c83d8bd117afcb025660eeb2d8283fe5b0995
snototter <snototter@users.noreply.github.com>
2020-02-25 18:34:51 +0100

    support bitwise operators for type-safe enums

---
+++ src/cpp/vcp_utils/enum_utils.h
@@ -0,0 +1,61 @@
+#ifndef __VCP_UTILS_ENUM_UTILS_H__
+#define __VCP_UTILS_ENUM_UTILS_H__
+
+#include <type_traits>
+#include "vcp_logging.h"
+
+// Provide bitwise operators for type-safe enums
+// Based on the excellent blog post: http://blog.bitwigglers.org/using-enum-classes-as-type-safe-bitmasks/
+
+template<typename Enum>
+Enum operator |(Enum lhs, Enum rhs)
+{
+    static_assert(std::is_enum<Enum>::value, "Template parameter is not an enum type");
+
+    using underlying = typename std::underlying_type<Enum>::type;
+
+    return static_cast<Enum> (
+        static_cast<underlying>(lhs) |
+        static_cast<underlying>(rhs)
+    );
+}
+
+template<typename Enum>
+Enum& operator |=(Enum& lhs, Enum rhs)
+{
+    static_assert(std::is_enum<Enum>::value, "Template parameter is not an enum type");
+
+    using underlying = typename std::underlying_type<Enum>::type;
+
+    lhs = static_cast<Enum> (
+        static_cast<underlying>(lhs) |
+        static_cast<underlying>(rhs)
+    );
+    return lhs;
+}
+//TODO add other .= overloads
+
+
+template<typename Enum>
+Enum operator &(Enum lhs, Enum rhs)
+{
+    static_assert(std::is_enum<Enum>::value, "Template parameter is not an enum type");
+
+    using underlying = typename std::underlying_type<Enum>::type;
+
+    return static_cast<Enum> (
+        static_cast<underlying>(lhs) &
+        static_cast<underlying>(rhs)
+    );
+}
+
+template<typename Enum>
+Enum operator ~(Enum rhs)
+{
+  static_assert(std::is_enum<Enum>::value, "Template parameter is not an enum type");
+  using underlying = typename std::underlying_type<Enum>::type;
+  return static_cast<Enum> (~static_cast<underlying>(rhs));
+}
+
+
+#endif // __VCP_UTILS_ENUM_UTILS_H__
