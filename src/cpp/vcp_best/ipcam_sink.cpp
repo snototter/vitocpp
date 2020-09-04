675480a08d621ea373529c63dea4594f61521db3
snototter <snototter@users.noreply.github.com>
2020-09-04 15:57:42 +0200

    switch to head-only requests for Mobotix API calls

--- src/cpp/vcp_best/ipcam_sink.cpp
+++ src/cpp/vcp_best/ipcam_sink.cpp
@@ -167,7 +167,7 @@ void MobotixDisableTextOverlay(const IpCameraSinkParams &p)
   if (!p.user.empty())
     url << p.user << ":" << p.password<< "@";
   url << p.host << "/control/control?section=text&textdisplay=disable";
-  if (vcp::best::curl::url_http_get(url.str().c_str(), 5) != 0)
+  if (vcp::best::curl::url_http_get(url.str().c_str(), 5, 1) != 0)
     VCP_LOG_FAILURE("Could not disable the text overlay on Mobotix camera: " << p);
 }
 
@@ -178,7 +178,7 @@ void MobotixSetPowerLineFrequency(const IpCameraSinkParams &p)
   if (!p.user.empty())
     url << p.user << ":" << p.password<< "@";
   url << p.host << "/control/control?section=exposurecontrol&ca_linefreq=50"; // 0 (No artificial light), 50 (Hz), 60 (Hz)
-  if (vcp::best::curl::url_http_get(url.str().c_str(), 5) != 0)
+  if (vcp::best::curl::url_http_get(url.str().c_str(), 5, 1) != 0)
     VCP_LOG_FAILURE("Could not enable anti-flickering on Mobotix camera: " << p);
 }
 
@@ -665,7 +665,10 @@ public:
     for (const auto &p : params_rtsp_)
     {
       if (p.ipcam_type == IpCameraType::Mobotix)
+      {
         MobotixDisableTextOverlay(p);
+        MobotixSetPowerLineFrequency(p);
+      }
     }
     return success;
   }
