ca62042e8e613fba9aa4f27e4d66155422fb8994
snototter <snototter@users.noreply.github.com>
2021-01-20 08:48:59 +0100

    bump version for opencv compatibility

--- src/cpp/vcp_utils/version.h
+++ src/cpp/vcp_utils/version.h
@@ -1,7 +1,7 @@
 #ifndef __VCP_VERSION_H__
 #define __VCP_VERSION_H__
-#define VCP_VERSION "1.0.3"
+#define VCP_VERSION "1.1.0"
 #define VCP_VERSION_MAJOR 1
-#define VCP_VERSION_MINOR 0
-#define VCP_VERSION_PATCH 0
+#define VCP_VERSION_MINOR 1
+#define VCP_VERSION_PATCH 1
 #endif // __VCP_VERSION_H__
