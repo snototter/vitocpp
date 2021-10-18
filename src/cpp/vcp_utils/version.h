7edb2a5ffd596ee0ebe0db97b2bc5acd476becff
snototter <snototter@users.noreply.github.com>
2021-10-18 21:35:14 +0200

    bump version

--- src/cpp/vcp_utils/version.h
+++ src/cpp/vcp_utils/version.h
@@ -1,7 +1,7 @@
 #ifndef __VCP_VERSION_H__
 #define __VCP_VERSION_H__
-#define VCP_VERSION "1.1.10"
+#define VCP_VERSION "1.2.0"
 #define VCP_VERSION_MAJOR 1
-#define VCP_VERSION_MINOR 1
-#define VCP_VERSION_PATCH 1
+#define VCP_VERSION_MINOR 2
+#define VCP_VERSION_PATCH 2
 #endif // __VCP_VERSION_H__
